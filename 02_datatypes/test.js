c4f689698d1dec93a56cf25735336aed3d158668
Mayur Thakor <mayurthakor@hotmail.com>
2023-12-17 03:32:25 +0000

    Datatypes

---
+++ 02_datatypes/test.js
@@ -0,0 +1,21 @@
+//Primitive datatypes
+const stableValue = "test1"
+var notSoStableValue = "test2"
+let scopedNotSoStableValue = "test3"
+randomValue = 5454
+
+//number
+//bigint
+//string
+//boolean   => true/false
+//null      => Standalone value
+//undefined => 
+//symbol    => To define uniquness
+
+//object
+
+console.log(typeof "test");
+console.table([stableValue.notSoStableValue,scopedNotSoStableValue,randomValue])
+
+console.log('typeof undefined is', typeof undefined);
+console.log('typeof null is', typeof null);
