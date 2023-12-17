591af10a00b656858f8932a6bd0fa1da75aa116a
Mayur Thakor <mayurthakor@hotmail.com>
2023-12-17 07:10:45 +0000

    Comparisions

---
+++ 04_comparision/test.js
@@ -0,0 +1,13 @@
+//Comparisons treat null as a value 0.
+//So the comparision operator <, <=, ==, >= works differently. 
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+//With Undefined, things are bit different.
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
