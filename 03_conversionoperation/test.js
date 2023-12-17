0b88c3f7d7392e6d08ad242755bb8a0ad8ba20df
Mayur Thakor <mayurthakor@hotmail.com>
2023-12-17 04:10:31 +0000

    Data Type Conversions

---
+++ 03_conversionoperation/test.js
@@ -0,0 +1,10 @@
+
+console.log("When simple string which is also a simple number written:- ", Number("33"));
+console.log("When tried to convert string with number into pure numbers:- ", Number("333simplestring"));
+console.log("When undefined value converted to number:- ", Number(undefined));
+console.log("When null value converted to number:- ", Number(null));
+console.log("When true value converted to number:- ", Number(true));
+console.log("When false value converted to number:- ", Number(false));
+
+console.log("When simple number 1 value converted to number:- ", Number(1));
+console.log("When simple number 0 value converted to number:- ", Number(0));
